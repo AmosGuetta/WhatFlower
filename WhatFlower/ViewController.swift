de745985fd8f16ac593a2118065787a3427f3b36
Amos Guetta <Amos.guetta.1990@gmail.com>
2018-09-28 14:08:26 +0300

    Icon app

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -20,8 +20,11 @@ class ViewController: UIViewController , UINavigationControllerDelegate,UIImageP
         imagePicker.allowsEditing = true
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let userImagePicker =  info[UIImagePickerControllerEditedImage] as? UIImage
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+        let userImagePicker =  info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage
         imageView.image = userImagePicker
         imagePicker.dismiss(animated: true, completion: nil)
     }
@@ -33,3 +36,13 @@ class ViewController: UIViewController , UINavigationControllerDelegate,UIImageP
     
 }
 
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
