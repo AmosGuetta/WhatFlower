bcce203795c6479ce7f899e945d3e2dea8aa16f9
Amos Guetta <Amos.guetta.1990@gmail.com>
2018-09-28 15:27:30 +0300

    Control view, and detect function

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import SwiftyJSON
+import Alamofire
+import SDWebImage
+import ColorThiefSwift
 
 class ViewController: UIViewController , UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
+    var pickedImage : UIImage?
+    
+
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -24,17 +37,54 @@ class ViewController: UIViewController , UINavigationControllerDelegate,UIImageP
 // Local variable inserted by Swift 4.2 migrator.
 let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
 
-        let userImagePicker =  info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage
-        imageView.image = userImagePicker
+        if let userImagePicker = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage {
+            
+            guard let ciImage = CIImage(image: userImagePicker) else {
+                fatalError("Could not convert image to CIImage.")
+            }
+            
+            pickedImage = userImagePicker
+            
+            
+            detect(flowerImage: ciImage)
+        }
+        
         imagePicker.dismiss(animated: true, completion: nil)
-    }
-    
-
-    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
-        present(imagePicker, animated: true, completion: nil)
-    }
     
 }
+    
+    func detect(flowerImage: CIImage) {
+        
+        
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Can't load model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not complete classfication")
+            }
+            
+            self.navigationItem.title = result.identifier.capitalized
+            
+            self.requestInfo(flowerName: result.identifier)
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+        
+    }
+    
+    
 
 
 // Helper function inserted by Swift 4.2 migrator.
